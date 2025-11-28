90b90af2258a7a8582cecda1de041994d1440ebd
nubranch <info@nubranch.ca>
2025-11-28 09:30:29 -0500

    Initial commit

---
+++ scripts/e2e.js
@@ -0,0 +1,72 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const spawn = require('child_process').spawnSync;
+
+const inCI = process.env.CI || false;
+
+function assert(truth, failure) {
+  if (!truth) {
+    throw new Error(failure);
+  }
+}
+function assertStatus(spawned) {
+  if (spawned.status !== 0) {
+    console.error('STDOUT', spawned.stdout.toString());
+    console.error('STDERR', spawned.stderr.toString());
+    process.exit(spawned.status);
+  }
+}
+
+const version = spawn('./bin/minimalcss.js', ['--version']);
+assertStatus(version);
+const versionNumber = version.stdout.toString();
+assert(!/[^\d\.]/.test(versionNumber.trim()), 'Not a version number');
+const versionNumber2 = spawn('./bin/minimalcss.js', ['-v']).stdout.toString();
+assert(versionNumber2 === versionNumber, 'alias not working');
+
+function openUrl(url, ...options) {
+  console.log(`Opening ${url} ...`);
+  if (inCI) {
+    options.push('--nosandbox');
+  }
+  options.push(url);
+  const t0 = new Date();
+  const opened = spawn('./bin/minimalcss.js', options);
+  assertStatus(opened);
+  const t1 = new Date();
+  const t = (t1 - t0) / 1000;
+  console.log(`Took ${t.toFixed(2)}s`);
+  return Promise.resolve(opened);
+}
+
+// Simplest form of opening
+openUrl('https://minimalcss.app/').then((spawned) => {
+  const css = spawned.stdout.toString();
+  assert(
+    css.length > 7000 && css.length < 10000,
+    'Expect CSS to be between 7,000...10,000'
+  );
+});
+
+// Open with -o
+openUrl('https://developer.mozilla.org/', '-o', '/tmp/mdn.css').then(
+  (spawned) => {
+    const stdout = spawned.stdout.toString();
+    assert(!stdout.trim(), 'Output should be empty');
+    const css = fs.readFileSync('/tmp/mdn.css').toString();
+    assert(
+      css.length > 10000 && css.length < 30000,
+      'Expect CSS to be between 10K...30K'
+    );
+  }
+);
+
+// With verbose output
+openUrl('https://developer.mozilla.org/', '--verbose').then((spawned) => {
+  const css = spawned.stdout.toString();
+  assert(/\/\*\nGenerated /.test(css), 'Expected verbose leading comment');
+  assert(
+    css.length > 10000 && css.length < 30000,
+    'Expect CSS to be between 10K...30K'
+  );
+});
---
+++ tests/utils.test.js
@@ -0,0 +1,53 @@
+const utils = require('../src/utils');
+
+test('Test unquoteString', async () => {
+  const f = utils.unquoteString;
+  expect(f('"peter"')).toEqual('peter');
+  expect(f(`'peter'`)).toEqual('peter');
+  // but leave it if it isn't balanced
+  expect(f('"peter')).toEqual('"peter');
+  // empty strings
+  expect(f('')).toEqual('');
+});
+
+test('Test reduceCSSSelector', async () => {
+  const f = utils.reduceCSSSelector;
+  // Most basic case.
+  expect(f('a:hover')).toEqual('a');
+  // More advanced case.
+  expect(f('a[href^="javascript:"]:after')).toEqual('a[href^="javascript:"]');
+  // Should work with ' instead of " too.
+  expect(f("a[href^='javascript:']:after")).toEqual("a[href^='javascript:']");
+});
+
+test('Test backslash selectors', async () => {
+  expect(utils.reduceCSSSelector('foo\\:hover')).toEqual('foo\\:hover');
+});
+
+test('Test parentSelectors', async () => {
+  const f = utils.getParentSelectors;
+  // Simplest possible
+  expect(f('.foo .bar')).toEqual(['.foo']);
+  // Slightly less simple
+  expect(f('.foo .bar .baz')).toEqual(['.foo', '.foo .bar']);
+  // Empty array
+  expect(f('.foo')).toEqual([]);
+  // Less trivial
+  expect(f('.ui.dropdown>.dropdown.icon:before')).toEqual(['.ui.dropdown']);
+  expect(f('.ui.vertical.menu .dropdown.item>.dropdown.icon:before')).toEqual([
+    '.ui.vertical.menu',
+    '.ui.vertical.menu .dropdown.item',
+  ]);
+  expect(
+    f(
+      '.ui.search.selection>.icon.input:not([class*="left icon"])>.icon~.remove.icon'
+    )
+  ).toEqual([
+    '.ui.search.selection',
+    '.ui.search.selection>.icon.input:not([class*="left icon"])',
+    '.ui.search.selection>.icon.input:not([class*="left icon"])>.icon',
+  ]);
+  expect(f('.ui[class*="right aligned"].search>.results')).toEqual([
+    '.ui[class*="right aligned"].search',
+  ]);
+});
